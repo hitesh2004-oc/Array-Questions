a3272005c5f3e6f6fa1aba5405712673e29afdf6
STM <STM@DESKTOP-U876HQ7>
2025-12-01 15:30:33 +0530

    Array Questions

---
+++ ArrayQuestion/src/ArrayQuestion/SumofArrayElements.java
@@ -0,0 +1,22 @@
+package ArrayQuestion;
+
+import java.util.Scanner;
+
+public class SumofArrayElements {
+	public static void main(String[] args) {
+	      Scanner sc = new Scanner(System.in);
+	      System.out.print("Enter Array Size: ");
+	      int n = sc.nextInt();
+	      int arr[] = new int[n];
+	      System.out.println("Enter Array Elements:-");
+	      for(int i=0;i<n;i++) {
+	    	  arr[i] = sc.nextInt();
+	      }
+	      int sum =0;
+	      for(int i=0; i<arr.length;i++) {
+	    	sum += arr[i];  
+	      }
+	      System.out.println("Sum of Array Elements:-");
+	      System.out.println(sum);
+	   }
+}
