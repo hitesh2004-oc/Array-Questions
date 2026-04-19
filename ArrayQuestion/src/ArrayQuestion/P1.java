b24d62677fc250f20f860c86706d18a369efc252
Hitesh Malviya <hiteshmalviya955@gmail.com>
2026-04-19 11:39:43 +0530

    2D Array Practice

---
+++ ArrayQuestion/src/ArrayQuestion/P1.java
@@ -0,0 +1,34 @@
+package ArrayQuestion;
+
+public class P1 {
+	
+	public static void main(String[] args) {
+		int arr[][] = {
+				{1,2,3},
+				{4,5,6},
+				{7,8,9}
+		};
+		for(int i = 0;i<arr.length;i++) {
+			for(int j = 0;j<arr[i].length;j++) {
+				int t = arr[i][j];
+				arr[i][j] = arr[j][i];
+				arr[j][i] = t;
+			}
+		}
+		for(int i = 0;i<arr.length;i++) {
+			for(int j = 0;j<arr[i].length;j++) {
+				System.out.print(arr[i][j]);
+			}
+			System.out.println();
+		}
+//		for(int i = 0;i<arr.length;i++) {
+//			for(int j = 0;j<arr[i].length;j++) {
+//			if(arr[i][j] == 4) System.out.println(true);
+//			else System.out.println(false);
+//			}
+//		}
+		
+		
+	}
+
+}
