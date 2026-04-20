aab4b013eb9c655ac43fce909f5d2ec4e3e074fd
Hitesh Malviya <hiteshmalviya955@gmail.com>
2026-04-20 07:53:00 +0530

    2D Array Practice

---
+++ ArrayQuestion/src/ArrayQuestion/P2.java
@@ -0,0 +1,17 @@
+package ArrayQuestion;
+
+import java.util.HashSet;
+
+public class P2 {
+	public static void main(String[] args) {
+		int arr[] = {1,2,6,8,9,5,6,6};
+		int a = arr.length;
+		int arr1[] = new int[a];
+		
+		for(int i = 0;i<arr.length;i++) {
+			boolean flag = true;
+			
+		}
+	
+  }
+}
