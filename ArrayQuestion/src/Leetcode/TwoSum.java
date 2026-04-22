7b0f0c9d22598c9ad0f8118cf55fbec9ec9b1134
Hitesh Malviya <hiteshmalviya955@gmail.com>
2026-04-22 10:51:42 +0530

    Refactor input handling in TwoSum class

--- ArrayQuestion/src/Leetcode/TwoSum.java
+++ ArrayQuestion/src/Leetcode/TwoSum.java
@@ -5,10 +5,8 @@ import java.util.Scanner;
 public class TwoSum {
 	public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter Array Size: ");
         int n = sc.nextInt();
-
         int[] arr = new int[n];
 
         System.out.println("Enter Elements:");
@@ -17,7 +15,6 @@ public class TwoSum {
         }
         System.out.print("Enter Target Value: ");
         int target = sc.nextInt();
-
         int count = 0;
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {  
