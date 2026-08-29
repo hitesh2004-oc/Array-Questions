bd74949ac7903501783ce1ff9bde79fecb87113a
Hitesh Malviya <hiteshmalviya955@gmail.com>
2026-08-29 11:53:11 +0530

    Leetcode Practice

--- ArrayQuestion/src/Leetcode/TwoSum.java
+++ ArrayQuestion/src/Leetcode/TwoSum.java
@@ -9,22 +9,25 @@ public class TwoSum {
         int n = sc.nextInt();
         int[] arr = new int[n];
 
+		// for User Input
         System.out.println("Enter Elements:");
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
         System.out.print("Enter Target Value: ");
         int target = sc.nextInt();
         int count = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {  
-                if (arr[i] + arr[j] == target) {
+
+		// Main Approach for TwoSum
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {  
+                if(arr[i] + arr[j] == target) {
                     System.out.println("["+i+", "+j+"]");
                     count++;
                 }
             }
         }  
-        if (count == 0) {
+        if(count == 0) {
             System.out.println("No Pair Found");
         }
     }
